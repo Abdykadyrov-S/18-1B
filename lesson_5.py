513ce34c3c3644176dd130081e981c825c86eb10
Abdykadyrov-S <abdykadyrovsyimyk0708@gmail.com>
2024-06-13 16:30:39 +0600

    update lesson_5.py

--- lesson_5.py
+++ lesson_5.py
@@ -19,6 +19,26 @@ git init
 git add lesson_5.py
 # git add .
 
+git commit -m "first commit"
+# git commit -m "lessons 1-5"
+
+git branch -M main
+
+git remote add origin <HTTPS link.git>
+# git remote add origin https://github.com/Abdykadyrov-S/18-1B.git
+
+git push -u origin <название вашей ветки>
+# git push -u origin main
+
+
+"""
+
+
+"""
+Команды для пуша на старый репозитроий
+
+git add .
+
 
 
 """
